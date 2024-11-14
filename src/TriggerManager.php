52ce53e13dad8c7d1a5bec35eaae71318bfba0d2
mitoop <me@mitoop.com>
2024-11-14 14:10:48 +0800

    adjust key

--- src/TriggerManager.php
+++ src/TriggerManager.php
@@ -6,19 +6,19 @@ class TriggerManager
 {
     protected $app;
 
-    public const TRIGGER_BIND_KEY = self::class.'@trigger_key';
+    public const BIND_KEY = 'mitoop.query.trigger';
 
     public function __construct($app)
     {
         $this->app = $app;
 
-        if (! $this->app->bound(self::TRIGGER_BIND_KEY)) {
-            $this->app->instance(self::TRIGGER_BIND_KEY, true);
+        if (! $this->app->bound(self::BIND_KEY)) {
+            $this->app->instance(self::BIND_KEY, true);
         }
     }
 
     public function hasTriggerConditionMet(): bool
     {
-        return $this->app->bound(self::TRIGGER_BIND_KEY) && $this->app->make(self::TRIGGER_BIND_KEY) === true;
+        return $this->app->bound(self::BIND_KEY) && $this->app->make(self::BIND_KEY) === true;
     }
 }
